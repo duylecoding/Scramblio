58262fa81dfebb6aab54f5bf51ca6f7881ba4836
Duy Le <duylezero@gmail.com>
2023-12-23 18:51:38 -0600

    stuff

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const MAX_WORDS = 4;
 
+function createBox(id, className, textContent) {
+  const box = document.createElement("div");
+  box.className = className;
+  box.id = id;
+  box.textContent = textContent;
+  return box;
+}
+
 function drawWordBank(container, word = "") {
   const row = document.createElement("div");
   row.id = `row`;
   row.className = "line";
   for (let i = 0; i < word.length; i++) {
-    const box = document.createElement("div");
-    box.className = "box";
-    box.id = `box/${i}/${word[i]}`;
-    box.textContent = word[i];
+    const box = createBox(
+      /* id= */ `box/${i}/${word[i]}`,
+      /* className= */ "box",
+      /* textContent= */ word[i]
+    );
     box.addEventListener("click", function (e) {
       const el = e.target;
       if (el.id !== "newLine") {
         el.parentNode.removeChild(el);
+        const split = el.id.split("/");
+        appendLetterToFirstAvailRow(split[split.length - 1]);
       }
     });
     row.appendChild(box);
@@ -23,21 +34,36 @@ function drawWordBank(container, word = "") {
 
 function appendRow() {
   const gridEl = document.getElementById("grid");
-  const row = document.createElement("div");
   const currentRows = document.getElementById("grid").children.length;
   if (currentRows >= MAX_WORDS) {
     return;
   }
-  row.id = `row/${currentRows}`;
-  const box = document.createElement("div");
-  box.className = "box";
-  box.textContent = "";
-  box.id = `row/${currentRows}/box/${0}`;
+  const row = document.createElement("div");
+  row.id = `row/${currentRows + 1}`;
+  row.className = "line";
+  const box = createBox(
+    /* id= */ `row/${currentRows + 1}/box/BLANK`,
+    /* className= */ "box",
+    /* textContent= */ ""
+  );
   row.appendChild(box);
   gridEl.appendChild(row);
 }
 
-function appendCharacterToFirstAvailRow() {}
+function appendLetterToFirstAvailRow(letter) {
+  const currentRows = document.getElementById("grid").children.length;
+  const row = document.getElementById(`row/${currentRows}`);
+  const blankEl = document.getElementById(`row/${currentRows}/box/BLANK`);
+  blankEl.parentNode.removeChild(blankEl);
+
+  const box = createBox(
+    /* id= */ `row/${currentRows}/box/${letter}`,
+    /* className= */ "box",
+    /* textContent= */ letter
+  );
+  row.appendChild(box);
+  row.appendChild(blankEl);
+}
 
 function shuffle(word) {
   const a = word.split(""),
